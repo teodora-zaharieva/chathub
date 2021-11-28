69d9a6c28a9795e19bb2e7777dd0d65cf3d9638a
Teodora <teodora.zaharieva@gtmhub.com>
2021-11-28 21:04:07 +0200

    added example for readline module

--- src/client.js
+++ src/client.js
@@ -1,6 +1,17 @@
 import readline from "readline";
 
-const rl = readline.createInterface({
+const io = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+});
+
+const messages = [];
+
+io.on("line", (data) => {
+  if (messages.length < 3) {
+    messages.push(data);
+  } else {
+    console.log(messages);
+    io.close();
+  }
 });
